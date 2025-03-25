d32ecbe6edc1305f6d8a639fd4ca28bd961aca58
Đặng Nhật Anh <awmtrg21@gmail.com>
2025-03-25 08:53:58 +0700

    chore: update file service and gitignore

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -6,7 +6,7 @@ export class FileService {
   private minioClient: Minio.Client;
   constructor() {
     this.minioClient = new Minio.Client({
-      endPoint: '1cfc-14-232-206-135.ngrok-free.app',
+      endPoint: '288b-2401-d800-91c1-d3f1-c449-6648-7c0a-aee2.ngrok-free.app',
       useSSL: true,
       accessKey: 'WuPiCTOhLJaIKz7LbzTK',
       secretKey: 'xz7WkmlpLsIwaUfzR38se1dJzSNg3wITXDYRJmVj',
