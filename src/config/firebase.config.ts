8d902f665b2f07819a5ed2f12927cc3e134ee598
cmc <vtcong@cmcglobal.vn>
2025-04-19 15:07:10 +0700

    add mount credential from server

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,7 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../cxxc-94c22-firebase-adminsdk-uf6vp-21626b7d43.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.applicationDefault(),
 });
 
 export default admin;
