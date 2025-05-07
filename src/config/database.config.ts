95f26fede394a48ddcae94cfa6a0bbd004b823b6
Nha An <dnhanh95@gmail.com>
2025-05-07 16:25:02 +0700

    temporary change to ip

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ const isCompiled = __filename.endsWith('.js');
 
 export const dbConfig: TypeOrmModuleOptions = {
   type: "postgres",
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '183.81.33.204',
   port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'cxxc',
   password: process.env.DB_PASSWORD || '6jie8yO8nt85',
