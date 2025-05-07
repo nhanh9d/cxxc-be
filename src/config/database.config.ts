47d4dad2a80dde99e394989566712b77813c4b0e
Nha An <dnhanh95@gmail.com>
2025-05-07 15:43:25 +0700

    update path

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const isCompiled = __filename.endsWith('.js');
+
 export const dbConfig: TypeOrmModuleOptions = {
   type: "postgres",
   host: process.env.DB_HOST || 'localhost',
@@ -7,7 +9,7 @@ export const dbConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME || 'cxxc',
   password: process.env.DB_PASSWORD || '6jie8yO8nt85',
   database: process.env.DB_DATABASE || 'cxxc',
-  entities: ['src/**/*.entity{.ts}'],
-  migrations: ['src/migrations/*{.ts}'],
+  entities: [isCompiled ? 'dist/src/**/*.entity{.js}' : 'src/**/*.entity{.ts}'],
+  migrations: [isCompiled ? 'dist/src/migrations/*{.js}' : 'src/migrations/*{.ts}'],
   autoLoadEntities: true,
 };
