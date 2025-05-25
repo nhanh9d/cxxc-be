2afca1280847b94ea57a4949d280605e2d563651
Nha An <dnhanh95@gmail.com>
2025-05-25 18:24:50 +0700

    add debug log

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as Minio from 'minio';
 import { SystemConfigService } from 'src/system-config/system-config.service';
 
 @Injectable()
 export class FileService {
   private readonly BE_CONFIG_KEY = 'be.config';
+  private readonly logger = new Logger(FileService.name);
   constructor(private readonly systemConfigService: SystemConfigService) {
   }
 
   async getPresignedUrl(userId: string, fileName: string) {
     const beConfig = await this.systemConfigService.getConfigValueByKey(this.BE_CONFIG_KEY);
+    this.logger.log(`BE Config: ${JSON.stringify(beConfig)}`);
+
     const minioClient = new Minio.Client({
       endPoint: beConfig.minio.endPoint,
       useSSL: beConfig.minio.useSSL,
@@ -18,7 +21,7 @@ export class FileService {
     });
 
     const url = await minioClient.presignedPutObject("user", `${userId}/${fileName}`);
-    console.log(url);
+    this.logger.log(`Presigned URL: ${url}`);
     return url;
   }
 }
