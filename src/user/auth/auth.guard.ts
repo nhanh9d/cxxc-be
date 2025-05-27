bd5212ee8d0292bf58316e2d683822c4f8237545
Nha An <dnhanh95@gmail.com>
2025-05-27 10:07:56 +0700

    add logs

--- src/user/auth/auth.guard.ts
+++ src/user/auth/auth.guard.ts
@@ -3,6 +3,7 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -10,10 +11,17 @@ import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { IS_PUBLIC_KEY } from 'src/shared/decorators/public.decorator';
+import { DiscordLogger } from 'src/shared/services/discord.log.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private configService: ConfigService, private reflector: Reflector) { }
+  private readonly logger = new Logger(AuthGuard.name);
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private reflector: Reflector,
+    private readonly discordLogger: DiscordLogger
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
@@ -28,6 +36,8 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
+      this.logger.log(`Token is not provided`);
+      this.discordLogger.log(`Token is not provided`);
       throw new UnauthorizedException();
     }
 
@@ -41,7 +51,9 @@ export class AuthGuard implements CanActivate {
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = payload;
-    } catch {
+    } catch (error) {
+      this.logger.log(`Error verifying token: ${error}`);
+      this.discordLogger.log(`Error verifying token: ${error}`);
       throw new UnauthorizedException();
     }
     return true;
