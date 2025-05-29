615366b20a644c6f1c29bf93181f9f0e95022a53
Nha An <dnhanh95@gmail.com>
2025-05-29 18:19:54 +0700

    fix dont have creator and add creator as a member of event

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { EditEventDto, EventDto, EventMemberDto, RegisterEventDto } from './dto/event.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Event } from './entity/event.entity';
@@ -6,16 +6,18 @@ import { Repository } from 'typeorm';
 import slugify from 'slugify';
 import { TokenInformationDto } from 'src/user/dto/token-info.dto';
 import { EventMember, MemberStatus, RoleType } from './entity/event-member.entity';
-import { User } from 'src/user/entity/user.entity';
 import { EventInvitation, InvitationStatus } from './entity/event-invitation.entity';
 import { RabbitMQService } from '../rabbitmq/rabbitmq.service';
 import { RMQ_PATTERNS } from '../rabbitmq/constants';
+import { UserService } from 'src/user/user.service';
+import { DiscordLogger } from '../shared/services/discord.log.service';
 
 @Injectable()
 export class EventService {
   /**
    *
    */
+  private readonly logger = new Logger(EventService.name);
   constructor(
     @InjectRepository(Event)
     private readonly eventRepository: Repository<Event>,
@@ -23,9 +25,9 @@ export class EventService {
     private readonly eventMemberRepository: Repository<EventMember>,
     @InjectRepository(EventInvitation)
     private readonly eventInvitationRepository: Repository<EventInvitation>,
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-    private readonly rabbitMQService: RabbitMQService
+    private readonly rabbitMQService: RabbitMQService,
+    private readonly userService: UserService,
+    private readonly discordLogger: DiscordLogger
   ) {
   }
 
@@ -34,7 +36,7 @@ export class EventService {
   }
 
   async getEventStatistic(id: number) {
-    const event = await this.eventRepository.findOne({ where: { id } });
+    const event = await this.eventRepository.findOne({ where: { id }, relations: ['creator'] });
 
     if (!event) {
       throw new Error('Không tìm thấy chuyến đi');
@@ -43,6 +45,8 @@ export class EventService {
     const invitedNo = await this.eventInvitationRepository.count({ where: { event } });
     const rejectedNo = await this.eventInvitationRepository.count({ where: { event, status: InvitationStatus.rejected } });
     const members = await this.eventMemberRepository.find({ where: { event }, relations: ['user'] });
+
+    this.discordLogger.log(`event detail: ${JSON.stringify(event)}`);
 
     return { event, invitedNo, rejectedNo, members }
   }
@@ -68,8 +72,16 @@ export class EventService {
     const event = await this.eventRepository.save({
       ...payload,
       slug,
-      creator: { id: user.sub }
+      creator: { id: user.sub },
     });
+
+    //add default member who is the creator
+    await this.eventMemberRepository.save({
+      status: MemberStatus.confirmed,
+      memberRole: RoleType.host,
+      user: { id: user.sub },
+      event: { id: event.id }
+    })
 
     // await this.rabbitMQService.emit(RMQ_PATTERNS.EVENT.CREATED, event);
     return event;
@@ -96,7 +108,7 @@ export class EventService {
   }
 
   async registerEvent(payload: RegisterEventDto, memberToken: TokenInformationDto) {
-    const user = await this.userRepository.findOneBy({ id: memberToken.sub });
+    const user = await this.userService.findById(memberToken.sub);
     if (!user) {
       throw new Error("Người dùng không tồn tại");
     }
