67b4aa40ac98460830afd99c7b4cdb70a0c86d5f
Nha An <dnhanh95@gmail.com>
2025-06-05 19:41:20 +0700

    add user to vehicle info

--- src/vehicle/vehicle.service.ts
+++ src/vehicle/vehicle.service.ts
@@ -3,6 +3,7 @@ import { CreateVehicleDto } from './dto/vehicle.create.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Vehicle } from './entity/vehicle.entity';
 import { Repository } from 'typeorm';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class VehicleService {
@@ -12,9 +13,11 @@ export class VehicleService {
   constructor(
     @InjectRepository(Vehicle)
     private readonly vehicleRepository: Repository<Vehicle>,
+    private readonly userService: UserService
   ) { }
   async createVehicle(vehicle: CreateVehicleDto) {
-    const entity = await this.vehicleRepository.save({ ...vehicle });
+    const user = await this.userService.findById(vehicle.userId);
+    const entity = await this.vehicleRepository.save({ ...vehicle, user });
     console.log("🚀 ~ VehicleService ~ createVehicle ~ entity:", entity)
 
     return entity;
